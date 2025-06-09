8a788c49bf2d3d42e1d00e0e4e61f34d1e1bc297
Idea Bosque <ideabosque@gmail.com>
2025-06-08 17:03:01 -0700

    Add AWS variables.

--- ai_mcp_daemon_engine/main.py
+++ ai_mcp_daemon_engine/main.py
@@ -81,6 +81,9 @@ def main():
                 if os.getenv("MCP_CONFIG_FILE")
                 else None
             ),
+            "region_name": os.getenv("region_name"),
+            "aws_access_key_id": os.getenv("aws_access_key_id"),
+            "aws_secret_access_key": os.getenv("aws_secret_access_key"),
         }
     )
     daemon.run()
