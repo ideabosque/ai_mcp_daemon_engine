688880a25a8dba283463876250ab759e8cc72139
Idea Bosque <ideabosque@gmail.com>
2025-06-16 19:06:42 -0700

    Add support for AWS Lambda.

--- ai_mcp_daemon_engine/handlers/mcp_server.py
+++ ai_mcp_daemon_engine/handlers/mcp_server.py
@@ -154,13 +154,13 @@ async def process_mcp_message(endpoint_id: str, message: Dict) -> Dict:
                 "id": msg_id,
                 "result": {
                     "resources": [
-                        {
-                            "uri": resource.uri,
-                            "name": resource.name,
-                            "description": resource.description,
-                            "mimeType": resource.mimeType,
-                        }
-                        for resource in resources
+                        # {
+                        #     "uri": resource.uri,
+                        #     "name": resource.name,
+                        #     "description": resource.description,
+                        #     "mimeType": resource.mimeType,
+                        # }
+                        # for resource in resources
                     ]
                 },
             }
