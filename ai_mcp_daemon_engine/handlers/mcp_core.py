7acd19be717ce7ca0ec9a5dea3b9a6bbc46dedc8
Idea Bosque <ideabosque@gmail.com>
2025-12-10 19:27:08 -0800

    Update mcp_core.py

--- ai_mcp_daemon_engine/handlers/mcp_core.py
+++ ai_mcp_daemon_engine/handlers/mcp_core.py
@@ -9,14 +9,24 @@ from typing import Any, Dict
 
 from graphene import Schema
 
-from silvaengine_dynamodb_base import SilvaEngineDynamoDBBase
+from silvaengine_dynamodb_base import BaseModel
+from silvaengine_utility import Graphql
 
 from .schema import Mutations, Query, type_class
 
 
-class MCPCore(SilvaEngineDynamoDBBase):
+class MCPCore(Graphql):
     def __init__(self, logger: logging.Logger, **setting: Dict[str, Any]) -> None:
-        SilvaEngineDynamoDBBase.__init__(self, logger, **setting)
+        Graphql.__init__(self, logger, **setting)
+
+        if (
+            setting.get("region_name")
+            and setting.get("aws_access_key_id")
+            and setting.get("aws_secret_access_key")
+        ):
+            BaseModel.Meta.region = setting.get("region_name")
+            BaseModel.Meta.aws_access_key_id = setting.get("aws_access_key_id")
+            BaseModel.Meta.aws_secret_access_key = setting.get("aws_secret_access_key")
 
         self.logger = logger
         self.setting = setting
@@ -32,4 +42,4 @@ class MCPCore(SilvaEngineDynamoDBBase):
             mutation=Mutations,
             types=type_class(),
         )
-        return self.graphql_execute(schema, **params)
+        return self.execute(schema, **params)
